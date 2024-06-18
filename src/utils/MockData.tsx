ba7104865f2d25b08c8b05568dc2a1acadbb85aa
anilvishwakarma-clecotech <anil.vishwakarma@clecotech.com>
2024-06-18 14:24:04 +0530

    Login mock data

---
+++ src/utils/MockData.tsx
@@ -0,0 +1,28 @@
+export const loginData=[
+    {
+        label:'Email',
+        flag:'email',
+        secureTextEntry:false,
+        keyBoardType:'',
+        error:false,
+        errorText:'Please provide valid email address',
+        suffixIcon:false,
+        prefixIcon:false
+    },
+    {
+        label:'Password',
+        flag:'password',
+        secureTextEntry:false,
+        keyBoardType:'',
+        error:false,
+        errorText:'Password must be greater than 8 characters',
+        suffixIcon:false,
+        prefixIcon:true
+    }
+]
+
+export const registerData=[
+    {
+        
+    }
+]
